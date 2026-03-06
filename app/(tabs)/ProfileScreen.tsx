5aa3c17243273a1d69625b926b83fedc51e7f97e
Ivin Jose <ivinjose@gmail.com>
2026-03-06 12:31:04 +0530

    NativeWind setup done

---
+++ app/(tabs)/ProfileScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function ProfileScreen() {
+    return (
+        // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }} >
+        <View className="flex-1 justify-center items-center bg-white" >
+            <Text>Profile</Text>
+        </View>
+    );
+}
