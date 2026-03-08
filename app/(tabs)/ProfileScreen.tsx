7273b9997e543ead1de330f606e9f18a6e1a91c6
Ivin Jose <ivinjose@gmail.com>
2026-03-08 13:02:49 +0530

    RNR setup done

--- app/(tabs)/ProfileScreen.tsx
+++ app/(tabs)/ProfileScreen.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from 'react-native';
+import { Button } from '@/components/ui/button';
+import { Text } from '@/components/ui/text';
+import { View } from 'react-native';
 
 export default function ProfileScreen() {
     return (
-        // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }} >
         <View className="flex-1 justify-center items-center bg-white" >
             <Text>Profile</Text>
+            <Button variant="destructive">
+                <Text>Click me</Text>
+            </Button>
         </View>
     );
 }
