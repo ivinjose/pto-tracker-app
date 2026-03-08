c35f9f9e0d1aa6db42704fe911f3dea88244c024
Ivin Jose <ivinjose@gmail.com>
2026-03-08 22:19:06 +0530

    (tabs)/_layout replaces ProtectedRoute

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,49 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
+
+import { Redirect, Tabs } from 'expo-router';
+import { ActivityIndicator, View } from 'react-native';
+import useAuth from '../../hooks/useAuth';
 
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function AppLayout() {
+	const colorScheme = useColorScheme();
+	const { auth, isLoading } = useAuth();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Bookings',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+	// 1. Show a loader while checking the token (Step 3 logic)
+	if (isLoading) {
+		return (
+			<View style={{ flex: 1, justifyContent: 'center' }}>
+				<ActivityIndicator size="large" />
+			</View>
+		);
+	}
+
+	// 2. If no token, redirect to Login (The ProtectedRoute logic)
+	if (!auth?.id) {
+		return <Redirect href="/(auth)/login" />;
+	}
+
+	// 3. If logged in, show the Tab Navigation
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+				headerShown: false,
+				tabBarButton: HapticTab,
+			}}>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: 'Home',
+					tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+				}} />
+			<Tabs.Screen name="manage" options={{ title: 'Manage' }} />
+			<Tabs.Screen name="train-bookings" options={{ title: 'Trains' }} />
+			<Tabs.Screen name="profiles" options={{ title: 'Profiles' }} />
+		</Tabs>
+	);
 }
